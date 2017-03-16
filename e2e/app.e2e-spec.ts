f0b196f59c2df6667a89cc610669dabbfd1a86dc
Angular CLI <angular-cli@angular.io>
2017-03-16 10:18:41 +0300

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { EvolutionPage } from './app.po';
+
+describe('evolution App', () => {
+  let page: EvolutionPage;
+
+  beforeEach(() => {
+    page = new EvolutionPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
