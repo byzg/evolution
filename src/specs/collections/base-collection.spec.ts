867f399f83a3ee7f93642d9a7a40ebb710a92719
byzg <byzg@mail.ru>
2017-04-07 12:34:11 +0300

    cover BaseCollection

--- src/specs/collections/base-collection.spec.ts
+++ src/specs/collections/base-collection.spec.ts
@@ -1,3 +1,4 @@
+import * as _ from 'lodash';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { BaseCollection } from '../../app/collections/base-collection';
@@ -5,6 +6,7 @@ import { BaseCollection } from '../../app/collections/base-collection';
 class StubBodyClass {}
 
 describe('BaseCollection', () => {
+  const localInject = (fn) => inject([BaseCollection], (service: BaseCollection<any>) => { fn(service); });
   beforeEach(() => {
     BaseCollection['bodyClass'] = StubBodyClass;
     TestBed.configureTestingModule({
@@ -12,7 +14,45 @@ describe('BaseCollection', () => {
     });
   });
 
-  it('should ...', inject([BaseCollection], (service: BaseCollection<any>) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('.constructor', () => {
+    it('should raise if there is no bodyClass', () => {
+      BaseCollection['bodyClass'] = null;
+      expect(() => { new BaseCollection(); }).toThrow();
+    });
+  });
+
+  describe('#push', () => {
+    it('should set bodyClass if it is still empty', localInject((service) => {
+      service.push();
+      expect(service.bodyClass).toEqual(StubBodyClass);
+    }));
+
+    it('should push given element', localInject((service) => {
+      service.push(73);
+      expect(service.includes(73)).toBe(true);
+    }));
+
+    it('should push new instance of bodyClass if pushing element is not given', localInject((service) => {
+      service.push();
+      expect(service[0].constructor).toEqual(StubBodyClass);
+    }));
+
+    it('should return length of itself', localInject((service) => {
+      expect(service.push()).toEqual(1);
+    }));
+  });
+
+  describe('#tick', () => {
+    it('should call tick for each element of collection', localInject((service) => {
+      spyOn(_, 'invokeMap');
+      service.tick();
+      expect(_.invokeMap).toHaveBeenCalledWith(service, 'tick');
+    }));
+  });
+
+  // describe('.bodyClass', () => {
+  //   it('should be equal to Plant class', () => {
+  //     expect(Plants.bodyClass).toEqual(Plant);
+  //   });
+  // });
 });
--- src/specs/factories/body.spec.ts
+++ src/specs/factories/body.spec.ts
@@ -1,6 +1,13 @@
 import { Body } from '../../app/factories/body';
 
 describe('Body', () => {
+  // const instance = () => {
+  //   if (!this._instance) {
+  //     this._instance = new Body();
+  //   }
+  //   return this._instance;
+  // };
+  // afterEach(() => this._instance = null);
   it('should ...', () => {
     expect(Body).toBeTruthy();
   });
