8b0c68ac6d4a14ad9a602f7afa9aea63e96152d6
byzg <byzg@mail.ru>
2017-04-10 12:28:15 +0300

    grow cover

--- src/specs/factories/body.spec.ts
+++ src/specs/factories/body.spec.ts
@@ -1,14 +1,30 @@
+import * as _ from 'lodash';
+
 import { Body } from '../../app/factories/body';
 
+class BodyChild extends Body {}
+class CellStub {}
+
 describe('Body', () => {
-  // const instance = () => {
-  //   if (!this._instance) {
-  //     this._instance = new Body();
-  //   }
-  //   return this._instance;
-  // };
-  // afterEach(() => this._instance = null);
-  it('should ...', () => {
-    expect(Body).toBeTruthy();
+  const instance = () => {
+    if (!this._instance) { this._instance = new BodyChild({}); }
+    return this._instance;
+  };
+  beforeEach(() => BodyChild['cellClass'] = CellStub);
+  afterEach(() => this._instance = null);
+
+  describe('.constructor', () => {
+    it('should raise if there is no cellClass', () => {
+      BodyChild['cellClass'] = null;
+      expect(() => { new BodyChild({}); }).toThrow();
+    });
+  });
+
+  describe('#tick', () => {
+    it('should call tick for each element of collection', (() => {
+      spyOn(_, 'invokeMap');
+      instance().tick();
+      expect(_.invokeMap).toHaveBeenCalledWith(instance().cells, 'tick');
+    }));
   });
 });
--- src/specs/factories/skill.spec.ts
+++ src/specs/factories/skill.spec.ts
@@ -1,7 +1,36 @@
+import { ServiceLocator } from '../../app/services/service-locator.service';
 import { Skill } from '../../app/factories/skill';
 
+class SkillChild extends Skill {}
+ServiceLocator.injector = ({get: () => {}});
+
 describe('Skill', () => {
-  it('should ...', () => {
-    expect(Skill).toBeTruthy();
+  const instance = (...args) => {
+    if (!this._instance) { this._instance = new SkillChild(args[0] || {}); }
+    return this._instance;
+  };
+  afterEach(() => this._instance = null);
+  describe('.constructor', () => {
+    it('should has injector', () => {
+      expect(instance().injector).toBe(ServiceLocator.injector);
+    });
+
+    it('should has owner as given parameter on constructor', () => {
+      const owner = {name: 'qwe'};
+      expect(instance(owner).owner).toBe(owner);
+    });
+  });
+
+  describe('.replaceName', () => {
+    it('should return name of class but without "skill" word', () => {
+    });
+  });
+
+  describe('#name', () => {
+    it('should call .replaceName', () => {
+      const spy = spyOn(Skill, 'replaceName');
+      instance().name;
+      expect(spy).toHaveBeenCalledWith('SkillChild');
+    });
   });
 });
