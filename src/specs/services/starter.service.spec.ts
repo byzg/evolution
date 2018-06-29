fbbf4b9b22cd8619ff259aa3d20ea2823e0ae3f5
Artem <byzgaev@artem-byzgaev.kodep.ru>
2018-06-29 10:38:04 +0300

    partly fix specs

--- src/specs/services/starter.service.spec.ts
+++ src/specs/services/starter.service.spec.ts
@@ -19,7 +19,29 @@ describe('StarterService', () => {
     });
   });
 
-  it('should ...', inject([StarterService], (service: StarterService) => {
-    expect(service).toBeTruthy();
-  }));
+  describe('.constructor', () => {
+    let watersPushSpy: jasmine.Spy;
+    let plantsPushSpy: jasmine.Spy;
+    let startTicksSpy: jasmine.Spy;
+    beforeEach(() => {
+      watersPushSpy = spyOn(Waters.prototype, 'push');
+      plantsPushSpy = spyOn(Plants.prototype, 'push');
+      startTicksSpy = spyOn(StarterService.prototype, 'startTicks')
+    });
+
+    it('should give water and plats to the world', inject([StarterService], (service: StarterService) => {
+      expect(watersPushSpy).toHaveBeenCalledTimes(10);
+      expect(watersPushSpy).toHaveBeenCalledTimes(10);
+    }));
+
+    it('should call startTicks', inject([StarterService], (service: StarterService) => {
+      expect(startTicksSpy).toHaveBeenCalledTimes(1);
+    }));
+
+    it('should fill his collection for ticks',
+      inject([StarterService, Waters], (service: StarterService, waters: Waters) => {
+        expect(service.collections[0]).toBe(waters);
+        expect(service.collections.length).toBe(1);
+    }));
+  });
 });
