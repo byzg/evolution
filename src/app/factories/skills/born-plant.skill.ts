f593f4aa32bd27249f66d8d4dd9059cbff24d29b
Artem <byzgaev@artem-byzgaev.kodep.ru>
2018-08-13 11:29:58 +0300

    grow cover

--- src/app/factories/skills/born-plant.skill.ts
+++ src/app/factories/skills/born-plant.skill.ts
@@ -13,9 +13,6 @@ export class BornPlantSkill extends Skill {
   boardService: BoardService = this.injector.get(BoardService);
   plants: Plants = ServiceLocator.injector.get(Plants);
   owner: CellWater;
-  constructor(owner) {
-    super(owner);
-  }
 
   run(): void {
     const vector = _.sample([[-1, 0], [0, -1], [1, 0], [0, 1]]);
@@ -23,12 +20,5 @@ export class BornPlantSkill extends Skill {
     if (this.boardService.isFreeSpace(plantCoords)) {
       this.plants.push(new Plant(plantCoords));
     }
-    // for (const vector of [[-1, 0], [0, -1], [1, 0], [0, 1]]) {
-    //   const plantCoords = {x: this.owner.x + vector[0], y: this.owner.y + vector[1] };
-    //   if (this.boardService.isFreeSpace(plantCoords)) {
-    //     this.plants.push(new Plant(plantCoords));
-    //     break;
-    //   }
-    // }
   }
 }
--- src/specs/factories/skills/born-plant.spec.ts
+++ src/specs/factories/skills/born-plant.spec.ts
@@ -1,7 +1,26 @@
+import * as _ from 'lodash-es';
+
 import { BornPlantSkill } from '../../../app/factories/skills/born-plant.skill';
+import { configureTestingModuleForFactory, hardSpyOn } from '../../support/spec-helper';
+import { BoardService } from '../../../app/services/board.service';
+import { Plants } from '../../../app/collections/alive/plants';
+import { CellWater } from '../../../app/factories/cells/cell-water';
+import { RndService } from '../../../app/services/rnd.service';
 
 describe('BornPlantSkill', () => {
-  it('should ...', () => {
-    expect(BornPlantSkill).toBeTruthy();
+  configureTestingModuleForFactory([BoardService, Plants, RndService]);
+  beforeEach(() => {
+    const owner = new CellWater({ x: 10, y: 10 });
+    this.instance = new BornPlantSkill(owner);
+  });
+
+  describe('#run', () => {
+    it('should add new plant to plants when board free in the place', ()=> {
+      hardSpyOn(_, 'sample').and.returnValue([0, 1]);
+      this.instance.run();
+      expect(this.instance.plants.length).toEqual(1);
+      this.instance.run();
+      expect(this.instance.plants.length).toEqual(1);
+    })
   });
 });
